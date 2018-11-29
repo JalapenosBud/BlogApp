f696717916429ba46369085abd1bb1e864639dd2
JalapenosBud <Jalapenosbud@gmail.com>
2018-11-29 20:21:01 +0100

    corrected sql

--- src/main/java/com/example/demo/dbstuff/db_create.sql
+++ src/main/java/com/example/demo/dbstuff/db_create.sql
@@ -1,26 +1,30 @@
+drop database if exists blogapp;
+create database if not exists blogapp;
+use blogapp;
+
 create table users
 (
-int id auto_increment,
+id int auto_increment,
 user_name varchar(25),
-password varchar(12)
+password varchar(12),
 
 primary key(id, user_name)
 );
 
 create table blogposts
 (
-int id primary key auto_increment,
+id int primary key auto_increment,
 title varchar(25),
 user_name_author varchar(25),
 creation_date date,
-blog_post_text varchar(max),
+blog_post_text blob,
 
 foreign key (user_name_author) references users(user_name)
 );
 
 create table tag_categories
 (
-int id primary key auto_increment,
+id int primary key auto_increment,
 tag_category_name varchar(25)
 );
 
